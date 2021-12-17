cb1c2da4d8ac60c84d7dad766045335268ca858d
xed-programmer <xedprogrammer@gmail.com>
2021-12-17 10:33:18 +0800

    Added nav bar in header.layout

--- app/resources/views/layouts/app/footer.layout.php
+++ app/resources/views/layouts/app/footer.layout.php
@@ -2,6 +2,14 @@
     use App\Classes\Page;    
 ?>
 
+<!-- container -->
+</div>
+<!-- content -->
+</div>
+<!-- content-wrapper -->
+</div>
+<!-- wrapper -->
+</div>
 <script src=<?php echo Page::asset('/public/plugins/jquery/jquery.min.js')?>></script>
 <script src=<?php echo Page::asset('/public/plugins/bootstrap/js/bootstrap.bundle.min.js')?>></script>
 <script src=<?php echo Page::asset('/public/dist/js/adminlte.js')?>></script>
